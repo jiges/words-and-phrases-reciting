b4521cd66e6c799bbb950540d5229b2643459476
jiges <ccr12312@163.com>
2019-04-26 15:56:20 +0800

    加入DTO和DO相互转换的工具

---
+++ words-and-phrases-reciting-server/src/main/java/com/ccr/wapr/server/configuration/DataConversionConfig.java
@@ -0,0 +1,17 @@
+package com.ccr.wapr.server.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ccr12312@163.com at 2019-4-26
+ */
+@Configuration
+public class DataConversionConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
